7bdb8fffed44c42a5212ea6baf27fbdb094f6323
Miracle275 <amatthews32508@gmail.com>
2026-01-08 15:24:24 -0500

    Added welcome activity and edit text to the name

--- app/src/main/java/org/baltimorecityschools/chessam/MainActivity.java
+++ app/src/main/java/org/baltimorecityschools/chessam/MainActivity.java
@@ -47,25 +47,25 @@ public class MainActivity extends AppCompatActivity {
 
         qTV = (TextView) findViewById(R.id.qTV);
 
-        q1 = new Question("The Queen is the most powerful piece in the board?", true);
+        q1 = new Question("The Queen is the most powerful piece in the board", true);
 
-        q2 = new Question("The knight is the only piece that can jump over other pieces?", true);
+        q2 = new Question("The knight is the only piece that can jump over other pieces", true);
 
-        q3 = new Question("Black always moves first?", false);
+        q3 = new Question("Black always moves first", false);
 
-        q4 = new Question("The king can be moved into check?", false);
+        q4 = new Question("The king can be moved into check", false);
 
-        q5 = new Question("A bishop can move backwards and forwards but not diagonally?", false);
+        q5 = new Question("A bishop can move backwards and forwards but not diagonally", false);
 
-        q6 = new Question("A checkmate immediately ends the game?", true);
+        q6 = new Question("A checkmate immediately ends the game", true);
 
-        q7 = new Question("The best move in chess is castling?", true);
+        q7 = new Question("Checkmate is better than castling", true);
 
-        q8 = new Question("There are more moves in chess than atoms in the universe?", true);
+        q8 = new Question("There are more moves in chess than atoms in the universe", true);
 
-        q9 = new Question("Early chess computers were made in the 1970s?", true);
+        q9 = new Question("Early chess computers were made in the 1970s", true);
 
-        q10 = new Question("The second book ever printed in English was about chess?", true);
+        q10 = new Question("The second book ever printed in English was about chess", true);
 
         questions = new Question[]{q1,q2,q3,q4,q5,q6,q7,q8,q9,q10};
 
--- app/src/main/java/org/baltimorecityschools/chessam/WelcomeActivity.java
+++ app/src/main/java/org/baltimorecityschools/chessam/WelcomeActivity.java
@@ -1,6 +1,10 @@
 package org.baltimorecityschools.chessam;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +14,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    Button nextBTN;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        nextBTN = (Button) findViewById(R.id.nextBTN);
+
+
+        nextBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent  welcome = new Intent(WelcomeActivity.this, MainActivity.class);
+                startActivity(welcome);
+            }
+        });
     }
 }
